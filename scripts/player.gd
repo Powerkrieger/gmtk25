8e69b4fa2d17a5c09a6d80bbbb805ea8e94e15cf
Powerkrieger <powerkrieger02@gmail.com>
2025-07-31 17:39:57 +0200

    change to Animation Player and Tree

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,68 +1,23 @@
 extends CharacterBody2D
 
-const SPEED = 100
-var current_dir = "none"
-@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+const SPEED: int = 100
+var input_dir: Vector2 = Vector2.ZERO
+@onready var animation_tree: AnimationTree = $AnimationTree
+
 
 func _ready():
-	anim.play("idle_down")
-
+	pass
+	
 func _physics_process(delta):
 	player_movement(delta)
 
-func player_movement(delta):
-	if Input.is_action_pressed("right"):
-		current_dir = "right"
-		play_anim(1)
-		velocity.x = SPEED
-		velocity.y = 0
-	elif Input.is_action_pressed("left"):
-		current_dir = "left"
-		play_anim(1)
-		velocity.x = -SPEED
-		velocity.y = 0
-	elif Input.is_action_pressed("down"):
-		current_dir = "down"
-		play_anim(1)
-		velocity.y = SPEED
-		velocity.x = 0
-	elif Input.is_action_pressed("up"):
-		current_dir = "up"
-		play_anim(1)
-		velocity.y = -SPEED
-		velocity.x = 0
-	else:
-		play_anim(0)
-		velocity.x = 0
-		velocity.y = 0
+func player_movement(_delta):
+	input_dir = Input.get_vector("left","right","up","down")
+	if input_dir:
+		velocity = input_dir * SPEED
+		animation_tree.set("parameters/StateMachine/MoveState/WalkState/blend_position", input_dir)
+		animation_tree.set("parameters/StateMachine/MoveState/IdleState/blend_position", input_dir)
+	else: 
+		velocity = Vector2.ZERO
+	
 	move_and_slide()
-
-
-
-func play_anim(movement):
-	var dir = current_dir
-
-	if dir == "right":
-		anim.flip_h = false
-		if movement == 1:
-			anim.play("walk_side")
-		elif movement == 0:
-			anim.play("idle_side")
-	if dir == "left":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("walk_side")
-		elif movement == 0:
-			anim.play("idle_side")
-	if dir == "down":
-		anim.flip_h = false
-		if movement == 1:
-			anim.play("walk_down")
-		elif movement == 0:
-			anim.play("idle_down")
-	if dir == "up":
-		anim.flip_h = false
-		if movement == 1:
-			anim.play("walk_up")
-		elif movement == 0:
-			anim.play("idle_up")
