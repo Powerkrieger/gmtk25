1c875627ac91c1e4dac62c4d4f3e0801e10d433a
Powerkrieger <powerkrieger02@gmail.com>
2025-07-31 22:25:07 +0200

    make destroyable crate

---
+++ scripts/crate.gd
@@ -0,0 +1,14 @@
+extends StaticBody2D
+
+const CRATE_EFFECT = preload("res://scenes/crate_effect.tscn")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func create_grass_effect() -> void:
+	var crate_effect = CRATE_EFFECT.instantiate()
+	get_tree().root.add_child(crate_effect)
+	crate_effect.global_position = global_position
+	queue_free()
+
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	create_grass_effect()
---
+++ scripts/crate_effect.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animated_sprite_2d.frame = 0
+	animated_sprite_2d.play("default")
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	queue_free()
