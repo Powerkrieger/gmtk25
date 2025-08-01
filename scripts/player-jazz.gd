0ac5eb780f2e86796f4af7465c92f10f67e1b170
Powerkrieger <powerkrieger02@gmail.com>
2025-08-01 13:45:55 +0200

    implement jazz-player, change project icon

---
+++ scripts/player-jazz.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+const SPEED: int = 140
+var input_dir: Vector2 = Vector2.ZERO
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var playback: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/StateMachine/playback")
+
+
+func _physics_process(delta):
+	var state = playback.get_current_node()
+	match state:
+		"MoveState": move_state(delta)
+		#"AttackState": pass
+
+func move_state(_delta) -> void:
+	input_dir = Input.get_vector("left","right","up","down")
+	if input_dir:
+		velocity = input_dir * SPEED
+		animation_tree.set("parameters/StateMachine/MoveState/WalkState/blend_position", input_dir)
+		animation_tree.set("parameters/StateMachine/MoveState/IdleState/blend_position", input_dir)
+		#animation_tree.set("parameters/StateMachine/AttackState/blend_position", input_dir)
+	else: 
+		velocity = Vector2.ZERO
+	#if Input.is_action_just_pressed("attack"):
+	#	playback.travel("AttackState")
+	move_and_slide()
